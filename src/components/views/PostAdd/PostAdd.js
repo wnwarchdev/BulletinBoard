3db02e4db8e5f8587a6c3cfcb547f67f11102e85
Wojtek Nowak <billnewmann@gmail.com>
2020-09-01 00:27:26 +0100

    Add logged verification to PostAdd component

--- src/components/views/PostAdd/PostAdd.js
+++ src/components/views/PostAdd/PostAdd.js
@@ -4,6 +4,7 @@ import Card from '@material-ui/core/Card';
 import Button from '@material-ui/core/Button';
 import { NavLink } from 'react-router-dom';
 import TextField from '@material-ui/core/TextField';
+import { NotFound } from '../NotFound/NotFound';
 
 import styles from './PostAdd.module.scss';
 
@@ -40,26 +41,32 @@ class Component extends React.Component {
 
 
   render() {
-    return (
-      <div className={styles.root}>
-        
-        <Card className={styles.card}>
-          <Button className={styles.goback} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} ><h3>&#8617;</h3></Button>
-          <p>Fill your post and your data:</p>
-          <TextField required id="title" name="title" label="Title" fullWidth value={this.state.title}  onChange={(e) => this.setState({ title: e.target.value })}/>
-          <br></br><br></br>
-          <TextField required id="text" name="text" label="I want to let others know..." multiline variant="outlined" rows={3} fullWidth value={this.state.text}  onChange={(e) => this.setState({ text: e.target.value })}/>
-          <TextField required id="author" name="author" label="Your name" fullWidth value={this.state.author}  onChange={(e) => this.setState({ author: e.target.value })}/>
-          <TextField required id="email" name="email" label="Email address" fullWidth value={this.state.email}  onChange={(e) => this.setState({ email: e.target.value })}/>
-          <TextField id="phone" name="phone" label="Phone number" fullWidth value={this.state.phone}  onChange={(e) => this.setState({ phone: e.target.value })}/>
-          <TextField required id="price" name="price" label="Price" fullWidth value={this.state.price}  onChange={(e) => this.setState({ price: e.target.value })}/>
-          <TextField required id="link" name="link" label="Photo link" fullWidth value={this.state.photo}  onChange={(e) => this.setState({ photo: e.target.value })}/>
-          <Button className={styles.submit} onClick={() => this.submit()}><h3>+</h3></Button>
-        </Card>
+    const { logged } = this.props;
+    //console.log (this.props.logged);
+    if (logged === true) {
+      return (
+        <div className={styles.root}>
+          
+          <Card className={styles.card}>
+            <Button className={styles.goback} component={NavLink} exact to={`${process.env.PUBLIC_URL}/`} ><h3>&#8617;</h3></Button>
+            <p>Fill your post and your data:</p>
+            <TextField required id="title" name="title" label="Title" fullWidth value={this.state.title}  onChange={(e) => this.setState({ title: e.target.value })}/>
+            <br></br><br></br>
+            <TextField required id="text" name="text" label="I want to let others know..." multiline variant="outlined" rows={3} fullWidth value={this.state.text}  onChange={(e) => this.setState({ text: e.target.value })}/>
+            <TextField required id="author" name="author" label="Your name" fullWidth value={this.state.author}  onChange={(e) => this.setState({ author: e.target.value })}/>
+            <TextField required id="email" name="email" label="Email address" fullWidth value={this.state.email}  onChange={(e) => this.setState({ email: e.target.value })}/>
+            <TextField id="phone" name="phone" label="Phone number" fullWidth value={this.state.phone}  onChange={(e) => this.setState({ phone: e.target.value })}/>
+            <TextField required id="price" name="price" label="Price" fullWidth value={this.state.price}  onChange={(e) => this.setState({ price: e.target.value })}/>
+            <TextField required id="link" name="link" label="Photo link" fullWidth value={this.state.photo}  onChange={(e) => this.setState({ photo: e.target.value })}/>
+            <Button className={styles.submit} onClick={() => this.submit()}><h3>+</h3></Button>
+          </Card>
 
-      </div>
-      
-    );
+        </div>
+        
+      );
+    } else {
+      return <NotFound />;
+    }
   }
 }
 
@@ -67,11 +74,13 @@ class Component extends React.Component {
 
 Component.propTypes = {
   addPost: PropTypes.any,
+  logged: PropTypes.bool,
 };
 
 const mapStateToProps = (state, props) => ({
   post: getPost(state, props.match.params.id),
   posts: getAll(state),
+  logged: state.logged,
 });
 
 const mapDispatchToProps = (dispatch) => ({
