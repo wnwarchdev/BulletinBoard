72f10a886a483a4a6f96729b04725a875bd9043e
Wojtek Nowak <billnewmann@gmail.com>
2020-09-09 21:27:38 +0100

    Fork models and controllers

---
+++ backend/controllers/posts.controller.js
@@ -0,0 +1,28 @@
+const Post = require('../models/post.model');
+
+exports.getPosts = async (req, res) => {
+  try {
+    const result = await Post
+      .find({status: 'published'})
+      .select('author created title')
+      .sort({created: -1});
+    if(!result) res.status(404).json({ post: 'Not found' });
+    else res.json(result);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+};
+
+
+exports.getPostById = async (req, res) => {
+  try {
+    const result = await Post
+      .findById(req.params.id);
+    if(!result) res.status(404).json({ post: 'Not found' });
+    else res.json(result);
+  }
+  catch(err) {
+    res.status(500).json(err);
+  }
+};
--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const router = express.Router();
 
-const Post = require('../models/post.model');
+//const Post = require('../models/post.model');
+const PostController = require('../controllers/posts.controller');
 
-router.get('/posts', async (req, res) => {
-  try {
-    const result = await Post
-      .find({status: 'published'})
-      .select('author created title')
-      .sort({created: -1});
-    if(!result) res.status(404).json({ post: 'Not found' });
-    else res.json(result);
-  }
-  catch(err) {
-    res.status(500).json(err);
-  }
-});
+// router.get('/posts', async (req, res) => {
+//   try {
+//     const result = await Post
+//       .find({status: 'published'})
+//       .select('author created title')
+//       .sort({created: -1});
+//     if(!result) res.status(404).json({ post: 'Not found' });
+//     else res.json(result);
+//   }
+//   catch(err) {
+//     res.status(500).json(err);
+//   }
+// });
 
-router.get('/posts/:id', async (req, res) => {
-  try {
-    const result = await Post
-      .findById(req.params.id);
-    if(!result) res.status(404).json({ post: 'Not found' });
-    else res.json(result);
-  }
-  catch(err) {
-    res.status(500).json(err);
-  }
-});
+// router.get('/posts/:id', async (req, res) => {
+//   try {
+//     const result = await Post
+//       .findById(req.params.id);
+//     if(!result) res.status(404).json({ post: 'Not found' });
+//     else res.json(result);
+//   }
+//   catch(err) {
+//     res.status(500).json(err);
+//   }
+// });
+
+router.get('/posts', PostController.getPosts);
+router.get('/posts/:id', PostController.getPostById);
 
 module.exports = router;
